fc83a6f08476d2d8ec62e31fb7bb188f95417c3c
Vinicius de Souza Silva <105164572+ViniciusSouzaSilva@users.noreply.github.com>
2025-02-22 18:17:04 -0300

    Create tipos_de_variaveis.js

---
+++ 22.02.2025/tipos_de_variaveis.js
@@ -0,0 +1,28 @@
+const ano = 1991;
+let atrasoEmSegundos = 0.00016;
+let area = (16*3.4);
+let metadeArea = area/2
+
+console.log("Ano: "+ano);
+console.log("Atraso em segundos: "+atrasoEmSegundos);
+console.log("Área: "+area);
+console.log("Tipo: "+typeof metadeArea);
+
+let a = 10; //decimal
+let b = 0x10; //hexadecimal
+let c = 0o10; //octal
+let d = 0b10; //binário
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+let x = 9e3;
+let y = 123e-5;
+
+console.log(x);
+console.log(y);
+
+let z = 0x21;
+console.log(z);
